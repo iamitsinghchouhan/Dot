25e892d930b53ececfd1d2c8fbeda9cb4099c73d
--unset <insidiouzzz@gmail.com>
2023-07-24 20:15:49 +0530

    constructor

--- JavaScript/JavaScript Basics II/index.js
+++ JavaScript/JavaScript Basics II/index.js
@@ -14,10 +14,10 @@
 
 // 1. Factory Function
 
-function createRectangle() {
+function createRectangle(length,breadth) {
     let rectangle = {
-        length:1,
-        breadth:2,
+        length:length,
+        breadth:breadth,
     
         draw: function (){
             console.log('draw rectangle');
@@ -27,7 +27,40 @@ function createRectangle() {
 }
 
 
-let rectangleObj1 = createRectangle();
-console.log(rectangleObj1);
+let rectangleObj1 = createRectangle(5,6);
+console.log(rectangleObj1.length);
 
 
+function Rectangle(){
+    this.length = 2;
+    this.breadth =3;
+    this.draw=function(){
+        console.log('draw');
+    }
+}
+
+
+let createRect = new Rectangle();
+console.log(createRect);
+
+
+createRect.color ='yellow';
+console.log(createRect);
+
+delete createRect.color;
+console.log(createRect);
+
+
+let Rectangle1 = new Function(
+    'length','breadth',
+    `this.length = length;
+     this.breadth =breadth;
+     this.draw=function(){
+        console.log('draw');
+    }`
+);
+
+// Object Creation using rectangle 1
+
+let rect = new Rectangle1(2 , 6);
+console.log(rect);
