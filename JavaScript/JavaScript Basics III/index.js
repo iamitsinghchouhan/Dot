f9ec72f9c64c41377d059bbc79c7bdc1d58dfad2
--unset <insidiouzzz@gmail.com>
2023-07-25 15:43:08 +0530

    Built-in_Object

---
+++ JavaScript/JavaScript Basics III/index.js
@@ -0,0 +1,36 @@
+//  Built-in Objects 
+
+// Math Function
+
+let a = Math.random(); //it will genrate a random number between 0 to 1.
+console.log(a);
+
+
+let b =Math.PI
+console.log(b);
+
+
+let c =Math.round(1.18545);
+console.log(c);
+
+
+let d =Math.max(23,14,554,578,6,484,5584,16516,31151,31,44);
+console.log(d);
+
+let e =Math.min(21,545,4851,2212,654,23,5,2,245,4212,2565,21,212,121,1212,11,2);
+console.log(e);
+
+
+// Creating a Object String
+
+let lastName = new String('Amit')
+
+let f = typeof(lastName);
+
+console.log(f);
+
+let message = 'this is my first message';
+
+let words = message.split(' ');
+
+console.log(words);
